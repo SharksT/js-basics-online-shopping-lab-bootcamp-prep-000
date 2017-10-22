efb4a9f48fc847c778ef30f507d0f1eb51d95d48
Felipe Passos <felipekaires@hotmail.com>
2017-10-22 14:13:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ function viewCart() {
         else{
           text = text + `, ${itemName[i]} at $${cart[i][itemName[i]]}`;
         }
-        
+
       }
       else if((cart.length - 1) == i) {
-          text = text + ` and ${itemName[i]} at $${cart[i][itemName[i]]}.`;
+          text = text + `, and ${itemName[i]} at $${cart[i][itemName[i]]}.`;
           //console.log(`${text} and ${itemName[i]} at $${cart[i][itemName[i]]}.`)
       }
     }
